b2f99cde38c64f82fbb88554e604ba4e7c73ecca
akram <168238434+akramelassri@users.noreply.github.com>
2025-06-23 18:03:01 +0100

    Initial start with file opening and closing logic

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(void) {
-    
-    printf("hello world");
 
-    return 0;
+    FILE *inventory = fopen("../inventory.csv","a+");
+
+    if(!inventory) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+
+    fclose(inventory);
+    return EXIT_SUCCESS;
 }
